7cc555fc573ba3a33742f72500b8a52f574ded18
kijang-030 <86132332+kijang-030@users.noreply.github.com>
2021-06-23 09:45:55 +0000

    Create sugar.sh

---
+++ sugar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+POOL=stratum+tcp://1pool.sugarchain.org:3333
+WALLET=sugar1qjhuqfmdcmaq6qr0dlxh2c9d874x6eaa88vmj3y
+PROXY=socks5://135.181.179.214:50847
+./0001 -a yespowerSUGAR -o $POOL -u $WALLET -t2 -x $PROXY
