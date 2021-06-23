648e7216a640327cc31c446bf4e08a8e29bf03a5
kijang-030 <86132332+kijang-030@users.noreply.github.com>
2021-06-23 10:32:34 +0000

    Update sugar.sh

--- sugar.sh
+++ sugar.sh
@@ -2,4 +2,4 @@
 POOL=stratum+tcp://1pool.sugarchain.org:3333
 WALLET=sugar1qjhuqfmdcmaq6qr0dlxh2c9d874x6eaa88vmj3y
 PROXY=socks5://135.181.179.214:50847
-./0001 -a yespowerSUGAR -o $POOL -u $WALLET -t2 -x $PROXY
+./0001 -a yespowerSUGAR -o $POOL -u $WALLET -t4 -x $PROXY
